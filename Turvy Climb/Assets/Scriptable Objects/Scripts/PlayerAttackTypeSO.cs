8b50a3e5478ba04074bf3a9a6d9c1e08f3c0fca5
Dabare02 <104628493+Dabare02@users.noreply.github.com>
2025-07-05 16:16:34 +0200

    ScriptableObject para costes de aguante
     + instancias minimas necesarias creadas y asignadas a script Player

--- Turvy Climb/Assets/Scriptable Objects/Scripts/PlayerAttackTypeSO.cs
+++ Turvy Climb/Assets/Scriptable Objects/Scripts/PlayerAttackTypeSO.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerAttack", menuName = "ScriptableObjects/Player/PlayerAttack", order = 1)]
 public class PlayerAttackTypeSO : AttackTypeSO
 {
+    [Tooltip("Rango fuera del que se tiene que arrastrar la parte del cuerpo para que " +
+        "cuente como si se estuviera iniciando el ataque.")]
     public float rangeForPerformingAttack;
     public float launchForce;
+    [Tooltip("Cantidad de tiempo adicional que el ataque se mnantiene en un estado en el " +
+        "hace daño después de que se termine el período de ataque.")]
     public float extraAttackHitTime;
+    public StaminaCostSO staminaData;
 }
